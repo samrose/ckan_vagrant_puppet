55ce4ce576068c59e0d136b9fd46044227872cce
Sam Rose <samrose@Sams-MacBook-Air.local>
2013-01-11 18:12:47 -0500

    right box

--- build.sh
+++ build.sh
@@ -50,8 +50,8 @@ echo ""
 cat > Vagrantfile <<EOF
 Vagrant::Config.run do |config|
   config.vm.host_name = '${vgrthostname}'
-  config.vm.box = "opscodelucid"
-  config.vm.box_url = "http://opscode-vagrant-boxes.s3.amazonaws.com/ubuntu10.04-gems.box"
+  config.vm.box = "lucid32"
+  config.vm.box_url = "lucid32 http://files.vagrantup.com/lucid32.box"
   config.vm.provision :shell, :inline => "/usr/bin/apt-get install -y puppet libaugeas-ruby augeas-tools rubygems"
   config.vm.provision :shell, :inline => "/usr/bin/apt-get update /usr/bin/apt-get install -y wget echo 'deb http://apt.ckan.org/ckan-1.MAJOR_VERSION lucid universe' | sudo tee /etc/apt/sources.list.d/ckan.list wget -qO- 'http://apt.ckan.org/packages_public.key' | /usr/bin/apt-key add - /usr/bin/apt-get update"	
   config.vm.provision :puppet, :module_path => "modules", :options => "--verbose" do |puppet|
